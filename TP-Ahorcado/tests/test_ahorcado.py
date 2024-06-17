dcabb272a3ca23357856b4ebd9a3a120d8aa3b41
Mora Kopech <morakopech@gmail.com>
2024-06-17 11:49:00 -0300

    rayita2 y 3

--- TP-Ahorcado/tests/test_ahorcado.py
+++ TP-Ahorcado/tests/test_ahorcado.py
@@ -3,3 +3,8 @@ from ahorcado import *
 def test_cant_rayitas():
     juego = Ahorcado()
     assert juego.rayitas("Botella") == "_ _ _ _ _ _ _"
+
+def test_cant_rayitas2():
+    juego = Ahorcado()
+    assert juego.rayitas("cargador") == "_ _ _ _ _ _ _ _"
+
