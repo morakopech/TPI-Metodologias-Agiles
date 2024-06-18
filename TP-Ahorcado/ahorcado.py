450ff641ebfc8c68128fdf3280037d6760e39248
Mora Kopech <morakopech@gmail.com>
2024-06-18 14:43:41 -0300

    Sprint 2

--- TP-Ahorcado/ahorcado.py
+++ TP-Ahorcado/ahorcado.py
@@ -3,10 +3,17 @@ import random
 class Ahorcado():
     def __init__(self):
         self.palabra_vacia = ""
+        self.palabra_a_adivinar = ""
     
     def rayitas(self, palabra):
         r = len(palabra)
         self.palabra_vacia = '_ ' * r
         return self.palabra_vacia.strip()  # Elimina el espacio extra al final de palabra_vacia
-        
     
+    def validar_letra(self, letra):
+        if (letra in self.palabra_a_adivinar): return True
+        else: return False
+    
+    def validar_palabra(self, palabra):
+        if (palabra == self.palabra_a_adivinar): return True
+        else: return False
--- TP-Ahorcado/tests/test_ahorcado.py
+++ TP-Ahorcado/tests/test_ahorcado.py
@@ -11,3 +11,24 @@ def test_cant_rayitas2():
 def test_cant_rayitas3():
     juego = Ahorcado()
     assert juego.rayitas("computadora") == "_ _ _ _ _ _ _ _ _ _ _"
+
+def test_letra_correcta():
+    juego = Ahorcado()
+    juego.palabra_a_adivinar = "hamaca"
+    assert juego.validar_letra("a") == True
+    
+def test_letra_incorrecta():
+    juego = Ahorcado()
+    juego.palabra_a_adivinar = "hamaca"
+    assert juego.validar_letra("e") == False
+    
+def test_palabra_correcta():
+    juego = Ahorcado()
+    juego.palabra_a_adivinar = "hamaca"
+    assert juego.validar_palabra("hamaca") == True
+    
+def test_palabra_incorrecta():
+    juego = Ahorcado()
+    juego.palabra_a_adivinar = "hamaca"
+    assert juego.validar_palabra("calesita") == False
+    
