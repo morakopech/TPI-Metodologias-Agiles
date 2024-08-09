114c4ab18e43da86e63b008cfa66e93ebf793a04
JuanCruzV4 <juancruz.vazquez87@hotmail.com>
2024-08-09 18:01:04 -0300

    a

--- TP-Ahorcado/app.py
+++ TP-Ahorcado/app.py
@@ -2,23 +2,22 @@ from flask import Flask, render_template, request, redirect, url_for
 from ahorcado import Ahorcado
 from dotenv import load_dotenv
 
-def crear_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    # Crear una instancia del juego
-    juego = Ahorcado()
+# Crear una instancia del juego
+juego = Ahorcado()
 
-    @app.route('/')
-    def elegir_dificultad():
+@app.route('/')
+def elegir_dificultad():
         return render_template('dificultad.html')
 
-    @app.route('/iniciar/<nivel>')
-    def iniciar_juego(nivel):
+@app.route('/iniciar/<nivel>')
+def iniciar_juego(nivel):
         juego.iniciar(dificultad=nivel)
         return redirect(url_for('inicio'))
 
-    @app.route('/inicio')
-    def inicio():
+@app.route('/inicio')
+def inicio():
         palabra = request.args.get('palabra')
         pista = request.args.get('pista')
         if palabra:
@@ -33,20 +32,18 @@ def crear_app():
             palabra_adivinada=juego.palabra_a_adivinar[0]
         )
 
-    @app.route('/intentar', methods=['POST'])
-    def intentar():
+@app.route('/intentar', methods=['POST'])
+def intentar():
         letra = request.form['letra'].lower()
         if not juego.juego_finalizado:
             juego.intento(letra)
             juego.validar_fin_del_juego()
         return redirect(url_for('inicio'))
 
-    @app.route('/reiniciar')
-    def reiniciar():
-        return redirect(url_for('elegir_dificultad'))
+@app.route('/reiniciar')
+def reiniciar():
+    return redirect(url_for('elegir_dificultad'))
     
-    return app
 
 if __name__ == '__main__':
-    app = crear_app()
     app.run()
