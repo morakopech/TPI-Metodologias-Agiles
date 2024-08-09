09f1db4b0dbb5f61c343702964b6b112517a8b56
JuanCruzV4 <juancruz.vazquez87@hotmail.com>
2024-08-09 17:11:11 -0300

    changes

--- TP-Ahorcado/Ahorcado-Proyect/app.py
+++ TP-Ahorcado/Ahorcado-Proyect/app.py
@@ -1,47 +1,52 @@
 from flask import Flask, render_template, request, redirect, url_for
 from ahorcado import Ahorcado
+from dotenv import load_dotenv
 
-app = Flask(__name__)
+def crear_app():
+    app = Flask(__name__)
 
-# Crear una instancia del juego
-juego = Ahorcado()
+    # Crear una instancia del juego
+    juego = Ahorcado()
 
-@app.route('/')
-def elegir_dificultad():
-    return render_template('dificultad.html')
+    @app.route('/')
+    def elegir_dificultad():
+        return render_template('dificultad.html')
 
-@app.route('/iniciar/<nivel>')
-def iniciar_juego(nivel):
-    juego.iniciar(dificultad=nivel)
-    return redirect(url_for('inicio'))
+    @app.route('/iniciar/<nivel>')
+    def iniciar_juego(nivel):
+        juego.iniciar(dificultad=nivel)
+        return redirect(url_for('inicio'))
 
-@app.route('/inicio')
-def inicio():
-    palabra = request.args.get('palabra')
-    pista = request.args.get('pista')
-    if palabra:
-        juego.iniciar(palabra=palabra, pista=pista)
-    return render_template(
-        'template.html',
-        palabra_a_mostrar=" ".join(juego.palabra_a_mostrar),
-        pista=juego.obtener_pista(),
-        intentos_restantes=juego.intentos_restantes,
-        letras_usadas=juego.letras_usadas,
-        juego_finalizado=juego.juego_finalizado,
-        palabra_adivinada=juego.palabra_a_adivinar[0]
-    )
+    @app.route('/inicio')
+    def inicio():
+        palabra = request.args.get('palabra')
+        pista = request.args.get('pista')
+        if palabra:
+            juego.iniciar(palabra=palabra, pista=pista)
+        return render_template(
+            'template.html',
+            palabra_a_mostrar=" ".join(juego.palabra_a_mostrar),
+            pista=juego.obtener_pista(),
+            intentos_restantes=juego.intentos_restantes,
+            letras_usadas=juego.letras_usadas,
+            juego_finalizado=juego.juego_finalizado,
+            palabra_adivinada=juego.palabra_a_adivinar[0]
+        )
 
-@app.route('/intentar', methods=['POST'])
-def intentar():
-    letra = request.form['letra'].lower()
-    if not juego.juego_finalizado:
-        juego.intento(letra)
-        juego.validar_fin_del_juego()
-    return redirect(url_for('inicio'))
+    @app.route('/intentar', methods=['POST'])
+    def intentar():
+        letra = request.form['letra'].lower()
+        if not juego.juego_finalizado:
+            juego.intento(letra)
+            juego.validar_fin_del_juego()
+        return redirect(url_for('inicio'))
 
-@app.route('/reiniciar')
-def reiniciar():
-    return redirect(url_for('elegir_dificultad'))
+    @app.route('/reiniciar')
+    def reiniciar():
+        return redirect(url_for('elegir_dificultad'))
+    
+    return app
 
 if __name__ == '__main__':
+    app = crear_app()
     app.run()
